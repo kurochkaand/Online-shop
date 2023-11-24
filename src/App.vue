a81bfb35a61072dd2ff1dcc60b25ac7d61f24b93
kutsenka <kutsenko1996@gmail.com>
2023-11-24 22:39:41 +0100

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import type SidebarVue from "./components/Sidebar.vue";
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
+  <Navigation />
   <SidebarVue />
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
