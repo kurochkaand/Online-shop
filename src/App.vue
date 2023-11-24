e16488ae5836e6bc8236694c692808a0fda22511
kurochkaand <kurochka.and@gmail.com>
2023-11-24 22:33:34 +0100

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import type SidebarVue from "./components/Sidebar.vue";
 </script>
 
 <template>
+  <SidebarVue />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
