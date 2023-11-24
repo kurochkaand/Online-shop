771a7efe4831abbc7beb802ed2dab6d97a2e5fd8
kutsenka <kutsenko1996@gmail.com>
2023-11-24 22:04:51 +0100

    Add Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Navigation />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import Navigation from "./components/Navigation.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Navigation,
+  },
 });
 </script>
 
