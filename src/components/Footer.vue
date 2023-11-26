95b4f8f1b6e1b3ff96271128be934f9f483223bf
AnnaMisiachna <anyamisia@gmail.com>
2023-11-26 17:10:32 +0100

    footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,31 @@
-<template>Footer</template>
+<template>
+  <div class="bottom-element">
+    <footer class="text-body-secondary text-center text-small">
+      <p class="mb-1">&copy; 2023 Crystals are medicine</p>
+      <ul class="list-inline">
+        <li class="list-inline-item"><a href="#">Terms and conditions</a></li>
+        <li class="list-inline-item"><a href="#">About us</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style>
+.bottom-element {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #f0f0f0a7;
+  padding: 20px;
+  margin: 0;
+  text-align: center;
+}
+p {
+  color: black;
+}
+li:hover {
+  text-decoration: underline;
+  text-decoration-color: rgb(102, 188, 122);
+}
+</style>
