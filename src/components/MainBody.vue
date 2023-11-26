9833b0470eadd0f6e4c8dfc065ebb26400aeb038
kutsenka <kutsenko1996@gmail.com>
2023-11-26 20:45:03 +0100

    App.vue old info deleted and MainBody.vue um and bm added

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -3,26 +3,44 @@
     <div class="row">
       <div class="container text-center">
         <h3>
-          Embark on a crystal journey with us! Discover the perfect crystal that resonates with your unique personality and
-          awakens your energy. Explore our collection now.
+          Embark on a crystal journey with us! Discover the perfect crystal that
+          resonates with your unique personality and awakens your energy.
+          Explore our collection now.
         </h3>
       </div>
     </div>
 
     <div class="row" style="height: 180px">
       <div class="col-sm-4">
-        <img src="old/amethyst_neck.jpg" class="img-fluid rounded" alt="amethyst_neck.jpg" />
+        <img
+          src="old/amethyst_neck.jpg"
+          class="img-fluid rounded"
+          alt="amethyst_neck.jpg"
+        />
         <p>AMETHYST necklace</p>
       </div>
       <div class="col-sm-4">
-        <img src="old/rose_quartz.jpg" class="img-fluid rounded" alt="rose_quartz.jpg" />
+        <img
+          src="old/rose_quartz.jpg"
+          class="img-fluid rounded"
+          alt="rose_quartz.jpg"
+        />
         <p>ROSE QUARTZ necklace</p>
       </div>
       <div class="col-sm-4">
-        <img src="old/citrine.jpg" class="img-fluid rounded" alt="citrine.jpg" />
+        <img
+          src="old/citrine.jpg"
+          class="img-fluid rounded"
+          alt="citrine.jpg"
+        />
         <p>CITRINE necklace</p>
       </div>
     </div>
   </div>
 </template>
-<style scoped></style>
+<style>
+.container {
+  margin-top: 100px;
+  margin-bottom: 200px;
+}
+</style>
