0d5734b590622ccf559fa5bdf75570ff938c873c
kurochkaand <kurochka.and@gmail.com>
2023-11-26 01:12:08 +0100

    main on and grid off

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1 class="row text-warning">This is main_text dfsdf</h1>
+  <div class="container">
+    <div class="row">
+      <div class="container text-center">
+        <h3>
+          Embark on a crystal journey with us! Discover the perfect crystal that resonates with your unique personality and
+          awakens your energy. Explore our collection now.
+        </h3>
+      </div>
+
+      <div class="row" style="height: 260px">
+        <div class="col-sm-4">
+          <img src="old/amethyst_neck.jpg" class="img-responsive" style="width: 100%; height: 180px" alt="amethyst_neck.jpg" />
+          <p>AMETHYST necklace</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="old/rose_quartz.jpg" class="img-responsive" style="width: 100%; height: 180px" alt="rose_quartz.jpg" />
+          <p>ROSE QUARTZ necklace</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="old/citrine.jpg" class="img-responsive" style="width: 100%; height: 180px" alt="citrine.jpg" />
+          <p>CITRINE necklace</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style scoped></style>
