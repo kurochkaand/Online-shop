3414fbb8dec6f7d341592c96e7c092d73a57883a
kurochkaand <kurochka.and@gmail.com>
2023-11-29 19:45:59 +0100

    Test2 new

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -22,18 +22,16 @@
             <li class="nav-item">
               <RouterLink class="nav-link" to="/test">Test</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/test2">Test2</RouterLink>
+            </li>
           </ul>
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" href="Log_in.html">Log in</a>
             </li>
             <li class="nav-item">
-              <a
-                class="btn btn-outline-light"
-                aria-current="page"
-                href="cart.html"
-                >Cart</a
-              >
+              <a class="btn btn-outline-light" aria-current="page" href="cart.html">Cart</a>
             </li>
           </ul>
         </div>
