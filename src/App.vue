afd4e3c173316b9c3ce482f98a9cae929863a899
kutsenka <kutsenko1996@gmail.com>
2023-11-29 19:15:06 +0100

    mainbody deleted

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Navigation from "./components/Navigation.vue";
 import Footer from "./components/Footer.vue";
-import MainBody from "./components/MainBody.vue";
 </script>
 
 <template>
