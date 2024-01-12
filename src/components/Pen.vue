25bbdaadaddb47e1eb89bb0efdd15dd369e620ac
AnnaMisiachna <anyamisia@gmail.com>
2024-01-12 20:41:28 +0100

    Pen file

---
+++ src/components/Pen.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  props: {
+    title: String,
+  },
+  setup(props) {
+    console.log(props.title);
+  },
+};
+</script>
+
+<template>
+  {{ $props.title }}
+</template>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
+import Pen from "./Pen.vue";
 
 // reactive state
 const count = ref(0);
@@ -9,7 +10,9 @@ const count = ref(0);
   <div>
     <button @click="count++">Count is: {{ count }}</button>
   </div>
+  <Pen :title="'New' + count + 'component'"></Pen>
 </template>
+
 <style scoped>
 div {
   padding-top: 100px;
